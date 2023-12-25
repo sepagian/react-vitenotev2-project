c2588cb4a5765496ab6a4d4523b5bc6fcb47237c
Rizky Dana <31857228+sepagian@users.noreply.github.com>
2023-12-26 01:17:58 +0800

    style: fix conditional styling

--- src/components/note/NoteList.tsx
+++ src/components/note/NoteList.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { Card, Flex, Grid, Text } from "@chakra-ui/react";
 import NoteCard from "./NoteCard";
 import AddNoteModal from "../modal/AddNoteModal";
 
@@ -11,18 +11,38 @@ interface Note {
 }
 type Props = {
   notes: Note[];
+  showAddButton: boolean;
   onDelete: (id: string) => void;
   onArchive: (id: string) => void;
   onRestore: (id: string) => void;
   onAdd: (note: { title: string; body: string }) => void;
 };
+
 const NoteList = (props: Props) => {
   return (
-    <SimpleGrid
-      columns={[1, 2, 3, 4]}
-      spacing={4}
-      p={4}>
-      <AddNoteModal addNote={props.onAdd} />
+    <Grid
+      templateColumns={[
+        "repeat(1, 1fr)",
+        "repeat(2, 1fr)",
+        "repeat(3, 1fr)",
+        "repeat(4, 1fr)",
+      ]}
+      p={4}
+      gap={4}>
+      {props.showAddButton && <AddNoteModal addNote={props.onAdd} />}
+      {props.notes.length === 0 && (
+        <Card
+          as={Flex}
+          p={8}
+          gap={2}
+          variant={"outline"}
+          bg={"gray.50"}
+          align={"center"}
+          height={{ base: "auto", sm: "150px", md: "240px", lg: "240px" }}
+          justifyContent={"center"}>
+          <Text fontSize={"lg"}>No note exist</Text>
+        </Card>
+      )}
       {props.notes.map((note) => (
         <NoteCard
           key={note.id}
@@ -32,7 +52,7 @@ const NoteList = (props: Props) => {
           {...note}
         />
       ))}
-    </SimpleGrid>
+    </Grid>
   );
 };
 
