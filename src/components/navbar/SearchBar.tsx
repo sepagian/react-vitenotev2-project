7a82987ade4116c6f56b78678ec491dcd19c6e43
Rizky Dana <31857228+sepagian@users.noreply.github.com>
2023-12-26 01:12:24 +0800

    feat: added filter note list by search input function

--- src/components/navbar/SearchBar.tsx
+++ src/components/navbar/SearchBar.tsx
@@ -1,14 +1,26 @@
 import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { MdSearch } from "react-icons/md";
+import React from "react";
+interface Props {
+  onSearchInput: (value: string) => void;
+}
 
-const SearchBar = () => {
+const SearchBar = ({ onSearchInput }: Props) => {
+  const handleInput: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const value = event.target.value;
+    onSearchInput(value);
+  };
+
   return (
     <Flex p={4}>
       <InputGroup>
         <InputLeftElement>
           <MdSearch size={25} />
         </InputLeftElement>
-        <Input placeholder="Search note" />
+        <Input
+          placeholder="Search note"
+          onChange={handleInput}
+        />
       </InputGroup>
     </Flex>
   );
