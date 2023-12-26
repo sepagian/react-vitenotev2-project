943d90b0d219a9bd77041cca6e0273e6a6853840
Rizky Dana <31857228+sepagian@users.noreply.github.com>
2023-12-26 14:57:42 +0800

    feat(navbar): moved searchbar to navbar component

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,29 +1,33 @@
 import { Divider, Flex, HStack, Heading, Image } from "@chakra-ui/react";
 import MenuLinks from "./MenuLinks";
 import MenuToggle from "./MenuToggle";
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   return (
-    <Flex
-      px={4}
-      pt={4}
-      justifyContent={"space-between"}>
-      <HStack>
-        <Image
-          boxSize={"32px"}
-          src="/vite.svg"
-          alt="Vite Logo"
-        />
-        <Heading
-          size="md"
-          fontWeight={"normal"}>
-          ViteNote V2
-        </Heading>
-        <Divider orientation="vertical" />
-        <MenuLinks />
-      </HStack>
-      <MenuToggle />
-    </Flex>
+    <>
+      <Flex
+        px={4}
+        pt={4}
+        justifyContent={"space-between"}>
+        <HStack>
+          <Image
+            boxSize={"32px"}
+            src="/vite.svg"
+            alt="Vite Logo"
+          />
+          <Heading
+            size="md"
+            fontWeight={"normal"}>
+            ViteNote V2
+          </Heading>
+          <Divider orientation="vertical" />
+          <MenuLinks />
+        </HStack>
+        <MenuToggle />
+      </Flex>
+      <SearchBar />
+    </>
   );
 };
 
