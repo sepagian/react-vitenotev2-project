a7c98e35703a08763da4b435b16b2915f49a9def
Rizky Dana <31857228+sepagian@users.noreply.github.com>
2023-12-26 15:05:22 +0800

    refactor(app): implemented custom store hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,35 @@
-import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
-import SearchBar from "./components/navbar/SearchBar";
 import NoteList from "./components/note/NoteList";
-import {
-  addNote,
-  archiveNote,
-  deleteNote,
-  getActiveNotes,
-  getAllNotes,
-  getArchivedNotes,
-  getNote,
-  unarchiveNote,
-} from "./utils/local-data";
 import NoteDetail from "./components/note/NoteDetail";
+import { useNoteStore, useSearchStore } from "./stores";
 
 function App() {
-  const [notes, setNotes] = useState(getAllNotes());
-  notes.sort((a, b) => {
-    return Date.parse(b.createdAt) - Date.parse(a.createdAt);
-  });
-
-  const onAddNote = (note: { title: string; body: string }) => {
-    addNote(note.title, note.body);
-    const updatedNotes = getAllNotes();
-    setNotes(updatedNotes);
-  };
-
-  const onArchiveNote = (id: string) => {
-    archiveNote(id);
-    const updatedNotes = getAllNotes();
-    setNotes(updatedNotes);
-  };
-
-  const onRestoreNote = (id: string) => {
-    unarchiveNote(id);
-    const updatedNotes = getAllNotes();
-    setNotes(updatedNotes);
-  };
-
-  const onDeleteNote = (id: string) => {
-    deleteNote(id);
-    const updatedNotes = getAllNotes();
-    setNotes(updatedNotes);
-  };
-
-  const [searchInput, setSearchInput] = useState("");
-
-  const filteredNotes = getActiveNotes().filter((note) => {
-    return note.title.toLowerCase().includes(searchInput.toLowerCase());
-  });
-
-  const archivedNotes = getArchivedNotes().filter((note) => {
-    return note.title.toLowerCase().includes(searchInput.toLowerCase());
-  });
+  const {
+    addNote,
+    archiveNote,
+    restoreNote,
+    deleteNote,
+    getArchivedNotes,
+    getActiveNotes,
+    getNoteDetail,
+  } = useNoteStore();
+  const { searchInput } = useSearchStore();
 
   return (
     <>
       <NavBar />
-      <SearchBar onSearchInput={setSearchInput} />
       <Routes>
         <Route
           path="/"
           element={
             <NoteList
-              notes={filteredNotes}
+              notes={getActiveNotes(searchInput)}
               showAddButton={true}
-              onAdd={onAddNote}
-              onArchive={onArchiveNote}
-              onDelete={onDeleteNote}
-              onRestore={onRestoreNote}
+              onAdd={addNote}
+              onArchive={archiveNote}
+              onDelete={deleteNote}
+              onRestore={restoreNote}
             />
           }
         />
@@ -77,18 +37,18 @@ function App() {
           path="/archived"
           element={
             <NoteList
-              notes={archivedNotes}
+              notes={getArchivedNotes(searchInput)}
               showAddButton={false}
-              onAdd={onAddNote}
-              onArchive={onArchiveNote}
-              onDelete={onDeleteNote}
-              onRestore={onRestoreNote}
+              onAdd={addNote}
+              onArchive={archiveNote}
+              onDelete={deleteNote}
+              onRestore={restoreNote}
             />
           }
         />
         <Route
           path="/:id"
-          element={<NoteDetail getNote={getNote} />}
+          element={<NoteDetail getNote={getNoteDetail} />}
         />
       </Routes>
     </>
